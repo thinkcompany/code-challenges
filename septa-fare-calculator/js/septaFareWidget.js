f922a848fd949704d532efa5c7d7a7b514d9c626
alexjhannan <alexjhannan@gmail.com>
2016-02-28 09:42:10 -0500

    Added event listeners to the widget

--- septa-fare-calculator/js/septaFareWidget.js
+++ septa-fare-calculator/js/septaFareWidget.js
@@ -1,10 +1,21 @@
+// pull data from fares.json
 $.ajax({
 	url: 'fares.json',
 	dataType: 'json',
-	success(data) {
-		console.log(data)
-	},
-	error(data) {
-		console.log(data)
+	error(err) {
+		console.log(err)
 	}
-});
+}).then(function(fares){
+	// on form input, update the fare
+	$("#septaFareWidget__form").on('input', updateFare)
+
+	// when a radio button changes, trigger input on the form (doesn't happen by default)
+	$("input[name=septaFareWidget__location").on('change', function(){
+		$("#septaFareWidget__form").trigger("input");
+	})
+
+	function updateFare() {
+		console.log('hi');
+	}
+
+})

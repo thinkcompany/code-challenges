b32388bdd624ee575562599277e8ee134a17eaba
John Merigliano <jmerigliano@gmail.com>
2016-03-14 16:21:15 -0400

    initial implementation

--- septa-fare-calculator/js/main.js
+++ septa-fare-calculator/js/main.js
@@ -1,23 +1,20 @@
-// The following is data pulled from fares.json
 var fares = {
-    // values to be populated from API (fares.json)
-    information: {},
+    // values to be populated from the API (fares.json)
+    when: [],
     zones: [],
-    when: ['Anytime', 'Weekday', 'Evening Weekend'],
-    purchaseLoc: ['Station Kisok', 'OnBoard'],
+    purchaseLocs: ['Station Kisok', 'OnBoard'],
     rides: 1,
     appStatus: 'Loading'
 };
 
 var viewModel = {
-    // dynamic fields from the fares model
-    information: ko.observable(),
+    // data and rendered choices
     zones: ko.observableArray(),
     appStatus: ko.observable(),
-    zoneNames: ko.observableArray(),
-    purchaseLoc: ko.observableArray(),
-    rides: ko.observable(),
     when: ko.observableArray(),
+    purchaseLocs: ko.observableArray(),
+    rides: ko.observable(),
+    currentPrice: ko.observable(),
 
     // app functions
     loadFares: function() {
@@ -27,22 +24,27 @@ var viewModel = {
 
         // the async request
         $.getJSON(faresJSON, function(d) {
-            var len = d.length;
+            var len = d.length,
+                when = [];
 
             // check for data
             if (len === 0) {
                 // update the status
                 viewModel.status('No data available');
             } else {
-                // add fares and zones information to the fares model
-                fares.information = d.info;
+                // add fares to the fares model
+
+                when = Object.keys(d.info);
+                when.pop(3);
+                when.pop(4);
+                fares.when = when;
                 fares.zones = d.zones;
 
                 // update the status
                 fares.appStatus = 'OK';
 
                 // populate the observables
-                viewModel.information(fares.information);
+                viewModel.when(fares.when);
                 viewModel.zones(fares.zones);
                 viewModel.appStatus(fares.appStatus);
 
@@ -54,28 +56,79 @@ var viewModel = {
             viewModel.appStatus('Failed to load resource.');
         });
     },
+    calculateFare: function(zone, when, where, rides) {
+        // static function:
+        // takes in zone, when, where, rides ->
+        // returns the fare
+        var all_zones = this.zones(),
+            index = zone - 1,
+            totalFare = 0;
 
-    getZones: function() {
-        // returns the available zones
-        var zones = fares.zones,
-            len = zones.length,
-            index = 0,
-            all_zones = [];
-
-        while (index < len) {
-            all_zones.push(zones[index].name);
-            index += 1;
+        if (when === 'weekday') {
+            if (where === '0') {
+                totalFare = all_zones[index].fares[0].price * rides;
+            } else {
+                totalFare = all_zones[index].fares[1].price * rides;
+            }
+        } else if (when === 'evening_weekend') {
+            if (where === '0') {
+                totalFare = all_zones[index].fares[2].price * rides;
+            } else {
+                totalFare = all_zones[index].fares[3].price * rides;
+            }
+        } else {
+            // anytime
+            totalFare = all_zones[index].fares[4].price * rides;
         }
 
-        return all_zones;
+        // display '0' as currency
+        if (totalFare % 1 === 0){
+            totalFare = totalFare + '.00';
+        }
+
+        return totalFare;
+    },
+
+    getFare: function() {
+        // returns the current fare price
+        var totalFare = 0,
+            currentZone = $('.zone').val(),
+            currentWhen = $('.when').val(),
+            currentWhere = $('.where').val(),
+            currentRides = parseFloat($('.rides').val());
+
+        // calculate the cost of the trip
+        switch(currentZone) {
+            case '1':
+                totalFare = this.calculateFare(1, currentWhen, currentWhere, currentRides);
+                break;
+            case '2':
+                totalFare = this.calculateFare(2, currentWhen, currentWhere, currentRides);
+                break;
+            case '3':
+                totalFare = this.calculateFare(3, currentWhen, currentWhere, currentRides);
+                break;
+            case '4':
+                totalFare = this.calculateFare(4, currentWhen, currentWhere, currentRides);
+                break;
+            case '5':
+                totalFare = this.calculateFare(5, currentWhen, currentWhere, currentRides);
+                break;
+        }
+
+        this.currentPrice(totalFare);
+        return totalFare;
+
     },
 
     init: function() {
         // populate some observables
-        this.zoneNames(this.getZones());
         this.when(fares.when);
-        this.purchaseLoc(fares.purchaseLoc);
+        this.purchaseLocs(fares.purchaseLocs);
         this.rides(fares.rides);
+
+        // calculate the intial price from the defaults
+        this.currentPrice(this.getFare());
     }
 };
 

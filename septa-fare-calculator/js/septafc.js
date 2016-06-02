c4dd656592a0db791661e6f3d0f9898bfb92fe9c
Donna Miller <donna@primetime.com>
2016-06-02 16:28:26 -0400

    fixed code

--- septa-fare-calculator/js/septafc.js
+++ septa-fare-calculator/js/septafc.js
@@ -1,44 +1,96 @@
 //global to hold JSON data
 var JSON_FARES="";
 
+//html for septra fare calculator widget
+var widgetHtml = function() {
+	return '<div class="fare-header-container">'+
+           '<h3>Regional Rails Fares</h3>'+
+         '</div>'+ 
+         
+         '<form id="sfform">' +
+         
+         '<div class="fare-container">'+
+           '<div id="farezone">'+
+             '<fieldset>'+
+               '<p>'+
+                 '<label for="sfzone"><b>Where are you going?</b></label>'+
+                 '<br><br>'+
+                 '<select id="sfzone">'+
+                 '</select>'+
+               '</p>'+
+             '</fieldset>'+
+           '</div>'+
+         '</div>'+         
+         '<div class="fare-container">'+
+           '<div id="faretype">'+
+            '<fieldset>'+
+               '<p>'+
+                 '<label for="sftype"><b>When are you riding?</b></label>'+
+                 '<br><br>'+
+                 '<select id="sftype">'+
+                 '</select>'+
+               '</p>'+
+             '</fieldset>'+
+             '<div class="fare-helper-text" id="type-helptext"></div>'+
+           '</div>'+
+         '</div>'+
+         
+         '<div class="fare-radio-container" >'+
+           '<div id="farepurchase">'+
+            '<fieldset>'+
+              '<legend><b>Where will you purchase the fare?<b></legend><br>'+
+                '<p class="fare-radio-btn">'+
+                  '<input type="radio" name="sfpurchase" id="advance_purchase" value="advance_purchase">'+
+                  '<label for="kiosk">Station Kiosk</label>'+
+                '</p>'+
+                '<p class="fare-radio-btn">'+
+                  '<input type="radio" name="sfpurchase" id="onboard_purchase" value="onboard_purchase">'+
+                  '<label for="onboard">Onboard</label>'+
+                '</p>'+
+                '<div class="fare-helper-text" id="purchase-helptext"></div>'+
+            '</fieldset>'+ 
+           '</div>'+
+         '</div>'+         
+         '<div class="fare-container">'+
+           '<fieldset>'+
+             '<p><label for="fareqty"><b>How many rides will you need?</b></label><br><br>'+
+             '<input type="number" id="fareqty" min=0 value=0></p>'+
+           '</fieldset>'+
+         '</div>'+
+         
+         '</form>' +
+         '<div class="fare-footer-container">'+
+            '<p><b>You fare will cost</b></p>'+           
+            '<div class="fare-total" id="totalfare"><b>$0.00</b></div>'+
+         '</div>';
+}
 
-//populate select options for zone, type options
-$.fn.populateCalcCombo = function(val, name) { 
-  var html = '';	
-  for (var i =0; i < val.length; i++) {
-    html += '<option value = "' + val[i] + '">'+ name[i] + '</option>';	
-  }
-  $(this).html(html);
-};
-
-
-// display help text for select type and radio purchase
-$.fn.displayText = function(type) {
-  if (type == 'anytime') {	
-    $(this).html(JSON_FARES.info.anytime); 
-  } else if (type == "weekday") {
-    $(this).html(JSON_FARES.info.weekday);
-  } else if (type == "evening_weekend") {
-    $(this).html(JSON_FARES.info.evening_weekend);
-  } else if (type == "advance_purchase") {
-    $(this).html(JSON_FARES.info.advance_purchase);
-  } else if (type == "onboard_purchase") {
-    $(this).html(JSON_FARES.info.onboard_purchase);
-} 
-};	
-
+//populate sfzone with names and zone values
+var typeHtml = function() {
+ var sftypeHtml = '';
+ var types = ['weekday', 'evening_weekend', 'anytime'];
+ for (var i=0; i < types.length; i++) {
+   sftypeHtml += '<option value = "' + types[i] + '">'+ types[i] + '</option>';
+  }  
+  return sftypeHtml;  
+}
 
 //calculate fare based on zone, type, purchase, and qty
-$.fn.calculateFare = function(zone, type, purchased, qty) {
+var calcFare = function(zone, type, purchased, qty) {
   var septaFare = 0;
   var numRides = qty;
   var extraRides = 0;
   
-  $.each(JSON_FARES.zones, function(index, value) {  		 
-    if (zone == value.zone) {
-       for (var i=0; i < value.fares.length;i++){	       
-         if (type === value.fares[i].type &&
-             purchased === value.fares[i].purchase) {
+  if (zone < 1 || zone > 5) {
+    alert("Invalid zone");
+    return 0;
+  }
+    
+  var value = JSON_FARES.zones[zone-1];
+  
+  for (var i=0; i < value.fares.length;i++){	       
+    if (type === value.fares[i].type &&
+        purchased === value.fares[i].purchase) {
           septaFare = value.fares[i].price;
           // use price for every number of trips
           // if exceeds number of trips for price, then increase
@@ -48,25 +100,20 @@ $.fn.calculateFare = function(zone, type, purchased, qty) {
           if (extraRides > 0) {            	    
             numRides++;
           } 
-          break;
-         }
-      }
-    }  
-  });
-  $(this).html('$'+(septaFare*numRides).toFixed(2));
+          return (septaFare*numRides);
+     }
+  }   
 };
 
-
-// recalc computed fare based on inputs
-function fare_recalc() {
-    var fzone = $('#sfzone').val();
-    var ftype = $('#sftype').val();    
-    var fpurchase = $('input[name=sfpurchase]:checked').val();
-    var fqty = $('#fareqty').val();    
-    
-    $('#totalfare').calculateFare(fzone, ftype, fpurchase, fqty); 
-}  
-
+//populate sfzone with names and zone values 
+var zoneHtml = function() {
+ var sfzoneHtml = '';          
+ 
+ $.each(JSON_FARES.zones, function(index, val) {
+       sfzoneHtml += '<option value = "' + val.zone + '">'+ val.name + '</option>';
+ });  
+  return sfzoneHtml;  
+}
 
 // get JSON via AJAX and store data in global variable JSON_FARES
 // populate select options for zones and types from JSON
@@ -77,35 +124,30 @@ function load_JSON() {
             dataType: 'json',
             success: function(data) {	
               JSON_FARES = data;
-              var fareZones =[];
-              var fareNames = [];
-              var fareType = [];              
+              //populate sfzone with JSON names and zone values  
+              $('#sfzone').html(zoneHtml());          
               
-              $.each(JSON_FARES.zones, function(index, value) {                		      
-                  fareZones.push(value.zone);
-                  fareNames.push(value.name);
-                  for (var i=0; i < value.fares.length; i++) { 
-                    if ($.inArray(value.fares[i].type, fareType) == -1) { 
-                      fareType.push(value.fares[i].type);
-                    }
-                  }
-              });     
-             $('#sfzone').populateCalcCombo(fareZones, fareNames);
-             $('#sftype').populateCalcCombo(fareType, fareType);
-            }	   
+            }  
+             
   });
 }
 
 
-// ready 
-$(document).ready(function () {
-		
-// read in JSON and populate select widgets		
-  load_JSON();
+//plugin septa fare calculator
+$.fn.septaFareCalculator = function() {
+//populate HTML	
+  this.html(widgetHtml);
   
+//populate sftype with fare type values  
+  $('#sftype').html(typeHtml); 
+		
+// read in JSON and populate zones 		
+  load_JSON();
+    
 // on change, display matching help text for selected type value
-  $('#sftype').on('change', function () {     		  
-    $('#type-helptext').displayText($(this).val());
+  $('#sftype').change(function () {     		  
+    //$('#type-helptext').displayText($(this).val());
+    $('#type-helptext').html(JSON_FARES.info[$(this).val()]);
    // anytime can only be an advance_purchase, so disable onboard 
    // and check advance_purchase
     if ($('#sftype').val() == "anytime") {
@@ -117,13 +159,36 @@ $(document).ready(function () {
   });  
   
 // on change, display matching help text for selected purchase value
-  $('#advance_purchase').on('change', function () {
-    $('#purchase-helptext').displayText($(this).val());
-  }); 
-  $('#onboard_purchase').on('change', function () {     		  
-    $('#purchase-helptext').displayText($(this).val());
-  });  
+ $('#sfform input[type=radio]').change(function () {
+    $('#purchase-helptext').html(JSON_FARES.info[$(this).val()]);
+  });
  
-});		
+ $('#sfform').change(function() {
+ // recalc computed fare based on inputs
+    var fzone = $('#sfzone').val();
+    var ftype = $('#sftype').val();    
+    var fpurchase = $('input[name=sfpurchase]:checked').val();
+    var fqty = $('#fareqty').val();    
+    
+    $('#totalfare').html('$' + calcFare(fzone, ftype, fpurchase, fqty).toFixed(2)); 
 
+  });  
+
+   return this;
+}		
+
+
+
+// ready 
+$(document).ready(function () {
+//septa fare calculator		
+  $('#septafc').septaFareCalculator();
+  
+});
+
+
+
+
+
+         
 

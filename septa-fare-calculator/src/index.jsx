f66722eba9200942a4d00629d97b4c55e1474ce8
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-07-31 02:58:30 -0400

    Converting App into class based component

--- septa-fare-calculator/src/index.jsx
+++ septa-fare-calculator/src/index.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
 
 import SeptaFareCalculator from './js/components/SeptaFareCalculator';
 import './scss/base.scss';
 
-const App = () => {
-    return (
-        <div>
-            <SeptaFareCalculator />
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SeptaFareCalculator />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#content'));

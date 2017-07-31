a7fd1e909490336b629076e0f82d814f27a666be
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-07-31 02:58:30 -0400

    Adding change event for zones

--- septa-fare-calculator/src/js/components/SeptaFareCalculator.jsx
+++ septa-fare-calculator/src/js/components/SeptaFareCalculator.jsx
@@ -9,15 +9,19 @@ class SeptaFareCalculator extends Component {
         this.state = {
             zone: 1,
             time: "weekday",
-            purchaseAt: "Onboard",
+            purchaseAt: "onboard_purchase",
             numRides: 1
         }
+    }
+
+    onZoneChange(newZone) {
+        this.setState({ zone: newZone });
     }
 
     render() {
         return (
             <div id="septa-fare-calculator">
-                <Destination zones={this.props.faresData.zones} />
+                <Destination zones={this.props.faresData.zones} zoneChange={this.onZoneChange.bind(this)} />
             </div>
         );
     }
--- septa-fare-calculator/src/js/components/WidgetComponents/Destination.jsx
+++ septa-fare-calculator/src/js/components/WidgetComponents/Destination.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Destination = ({ zones }) => {
+const Destination = ({ zones, zoneChange }) => {
     let selectOptions = zones.map((zone, idx) => {
         return <option value={zone.zone} key={idx}>{zone.name}</option>
     });
@@ -8,7 +8,7 @@ const Destination = ({ zones }) => {
     return (
         <div className="destination-container">
             <h1>Where are you going?</h1>
-            <select name="destination">
+            <select name="destination" onChange={ event => zoneChange(event.target.value) }>
                 {selectOptions}
             </select>
         </div>

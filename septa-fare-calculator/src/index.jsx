835e9213066d525c6768de8124507c75d48e8652
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2017-07-31 02:58:30 -0400

    Fetching fares from data

--- septa-fare-calculator/src/index.jsx
+++ septa-fare-calculator/src/index.jsx
@@ -9,6 +9,21 @@ class App extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            faresData: {}
+        };
+
+        this.fetchFares();
+    }
+
+    fetchFares() {
+        fetch('/_data/fares.json')
+          .then((response) => {
+            return response.json()
+          }).then((json) => {
+            console.log(json);
+            this.setState({ faresData: json });
+          });
     }
 
     render() {

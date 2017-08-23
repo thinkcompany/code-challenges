09883fae496371b752e28da1ca49244c875330b2
Uladzislau Sasnouski <mentalhook@gmail.com>
2017-08-23 19:50:18 -0400

    Build Purchase component responsible for purchase location

--- septa-fare-calculator/fare-calculator.jsx
+++ septa-fare-calculator/fare-calculator.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Zones from './zones';
 import Timings from './timings';
+import Purchase from './purchase';
 
 class FareCalculator extends React.Component {
   constructor(props) {
@@ -10,12 +11,17 @@ class FareCalculator extends React.Component {
     this.state = {
       zone: "1",
       time: "weekday",
-      purchase: "advance-purchase",
+      purchase: "advance_purchase",
       rides: 1,
     };
 
     this.updateZone = this.updateZone.bind(this);
     this.updateTime = this.updateTime.bind(this);
+    this.updatePurchase = this.updatePurchase.bind(this);
+  }
+
+  updatePurchase(e) {
+    this.setState({ purchase: e.currentTarget.value })
   }
 
   updateTime(e) {
@@ -29,11 +35,15 @@ class FareCalculator extends React.Component {
   render() {
     const { faresData } = this.props;
     if (!faresData.zones) return null;
-
+    
     return(
       <div className="calculator-container">
         <Zones availableZones={faresData.zones} updateZone={this.updateZone} />
         <Timings timeInfo={faresData.info[this.state.time]} updateTime={this.updateTime} />
+        <Purchase
+          purchaseInfo={faresData.info[this.state.purchase]}
+          selected={this.state.purchase}
+          updatePurchase={this.updatePurchase} />
       </div>
     );
   }

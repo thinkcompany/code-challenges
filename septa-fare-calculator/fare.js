f717521b65f5182f7cdd4b2514a544af13bb5637
littlewolff <jameschriswolff@gmail.com>
2017-11-03 15:12:51 -0400

    added scss and helper text to js and html

--- septa-fare-calculator/fare.js
+++ septa-fare-calculator/fare.js
@@ -2,10 +2,12 @@
 var regional_rail_trips = [];	//information about zones, tickets, prices.
 var regional_rail_info = [];	//information for help text.
 
+//Vaious input fields from the form.
 var regional_rail_zone_input;
 var regional_rail_time_input;
 var regional_rail_purchase_input;
 var regional_rail_ride_input;
+//Price field used to show results
 var regional_rail_ride_price;
 
 jQuery(document).ready(function() {
@@ -42,21 +44,27 @@ function set_regional_rail_change_events(){
 	});
 
 	jQuery(regional_rail_time_input).on('change', function(){
+
+		jQuery('#regional-rail-help-text-time').text(regional_rail_info[regional_rail_time_input.val()]);
 		check_regional_rail_price();
 	});
 
 	jQuery(regional_rail_purchase_input).on('change', function(){
+
+		jQuery('#regional-rail-help-text-purchase').text(regional_rail_info[jQuery('input[name=regional-rail-purchase]:checked').val()]);
 		check_regional_rail_price();
 	});
 
 	jQuery(regional_rail_ride_input).on('change', function(){
 		check_regional_rail_price();
 	});
+
+	check_regional_rail_price();
 }
 
 function check_regional_rail_price() {
 
-
+	//Check if anytime is selected if so limmit purchase to 10
 	if(regional_rail_time_input.val() == 'anytime'){
 		jQuery('#regional-rail-advanced').attr("checked", true);
 		regional_rail_ride_input.val(10);
@@ -68,55 +76,51 @@ function check_regional_rail_price() {
 		regional_rail_ride_input.attr("disabled", false);
 	}
 
+	//read in values of inputs
 	var regional_zone_number = regional_rail_zone_input.val();
 	var regional_type = regional_rail_time_input.val();
 	var regional_purchase = regional_rail_purchase_input.val();
 	var regional_rides = regional_rail_ride_input.val();
-
 	var regional_trip_cost = '----';
 
-	/*
-	 * Iterate through all zones to find the one matching the users.
-	 *
-	 * Side note: this could also be accomplished be reffering to 
-	 * regional_rail_trips[regional_zone_num-1] but I wanted to include 
-	 * this if more zones are added in the future.
-	 */
-	 regional_rail_trips.forEach(function(regional_zone, regional_zone_index){
-		if(regional_zone.zone == regional_zone_number){
-			//iterate through each fare for the correct price.
-			regional_zone.fares.forEach(function(regional_fare, regional_fare_index){
-				//when ride type and purchase location match return price
-				
-				console.log(regional_type);
+	//only check if rides has a positive value
+	 if(regional_rides != 0){
+	 	/*
+		 * Iterate through all zones to find the one matching the users.
+		 *
+		 * Side note: this could also be accomplished be reffering to 
+		 * regional_rail_trips[regional_zone_num-1] but I wanted to include 
+		 * this if more zones are added in the future.
+		 */
+		 regional_rail_trips.forEach(function(regional_zone, regional_zone_index){
+			if(regional_zone.zone == regional_zone_number){
+				//iterate through each fare for the correct price.
+				regional_zone.fares.forEach(function(regional_fare, regional_fare_index){
+					//when ride type and purchase location match return price
 
-				if(regional_fare.type == regional_type && regional_fare.purchase == regional_purchase){
-					/*
-					 * Check if anytime package of 10 tickets. 
-					 * If so return package price otherwise calculate the price.
-					 */
+					if(regional_fare.type == regional_type && regional_fare.purchase == regional_purchase){
+						/*
+						 * Check if anytime package of 10 tickets. 
+						 * If so return package price otherwise calculate the price.
+						 */
 
-					if (regional_type == "anytime"){
-						regional_trip_cost = regional_fare.price;
+						if (regional_type == "anytime"){
+							regional_trip_cost = regional_fare.price;
+						}
+						else{
+							regional_trip_cost = regional_fare.price * regional_rides;
+						}
 					}
-					else{
-						regional_trip_cost = regional_fare.price * regional_rides;
-					}
-				}
-			});
-		}
-	});
+				});
+			}
+		});
+	}
 
 	//return result in poper format or empty
 	if(regional_trip_cost != '----')
 		regional_trip_cost = '$' + Number(regional_trip_cost).toFixed(2);
 
 
-	//Update the current value of 
-	update_regional_rail_price(regional_trip_cost);
-}
-
-function update_regional_rail_price(price){
-	console.log(price);
-	regional_rail_ride_price.text(price);
+	//Update the current value of price
+	regional_rail_ride_price.text(regional_trip_cost);
 }

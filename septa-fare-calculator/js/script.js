896b182593d130ef6643b6f7752881245175cf9b
VinceOmega <vince.omega@gmail.com>
2017-11-05 20:48:02 -0500

    Finished. I didn't added the help text for the departure select because it was actually too large for the suggested size of the app (via the png supplied).

--- septa-fare-calculator/js/script.js
+++ septa-fare-calculator/js/script.js
@@ -1,55 +1,79 @@
 window.addEvent('domready', function(){
 
-	var info 	= {};
-	var zone_1 	= [];
-	var zone_2 	= [];
-	var zone_3 	= [];
-	var zone_4 	= [];
-	var zone_5 	= [];
+	$('zone_select').addEvent('change', function(e){
+		var zone_value 		= 	e.target.value;
+		var departure_value = 	$('departure_select').get('value');
+		var purchase_value 	= 	$$("input[type='radio']:checked").map(function(e){ return e.value } )[0];
+		var trips_value 	= 	$("numeric").get('value');
+		calculateCost(zone_value, departure_value, purchase_value, trips_value);
+	});
+
+	$('departure_select').addEvent('change', function(e){
+		var departure_value = 	e.target.value;
+		var zone_value 		= 	$('zone_select').get('value');
+		var purchase_value 	= 	$$("input[type='radio']:checked").map(function(e){ return e.value } )[0];
+		var trips_value 	= 	$("numeric").get('value');
+		calculateCost(zone_value, departure_value, purchase_value, trips_value);
+	});
+
+
+	$('onboard_purchase').addEvent('click', function(e){
+		var purchase_value 	= 	e.target.value;
+		var zone_value 		= 	$('zone_select').get('value');
+		var departure_value = 	$('departure_select').get('value');
+		var trips_value 	= 	$("numeric").get('value');
+		calculateCost(zone_value, departure_value, purchase_value, trips_value);
+
+	});
+
+	$('advance_purchase').addEvent('click', function(e){
+		var purchase_value 	= 	e.target.value;
+		var zone_value 		= 	$('zone_select').get('value');
+		var departure_value = 	$('departure_select').get('value');
+		var trips_value 	= 	$("numeric").get('value');
+		calculateCost(zone_value, departure_value, purchase_value, trips_value);
+	});
+
+	$('numeric').addEvent('keyup', function(e){
+		var trips_value 	= 	e.target.value;
+		var zone_value 		= 	$('zone_select').get('value');
+		var departure_value = 	$('departure_select').get('value');
+		var purchase_value 	= 	$$("input[type='radio']:checked").map(function(e){ return e.value } )[0];
+		calculateCost(zone_value, departure_value, purchase_value, trips_value);
+
+	});
+
+});
+
+var app = new Object();
+app.zone = 0;
+app.purchase = '';
+app.type = '';
+app.trip = 1;
+
+function calculateCost(zones, types, purchases, trips){
+
+	app.zone 		= zones - 1;
+	app.purchase 	= purchases;
+	app.type 		= types;
+	app.trip 		= trips;
 
 	var server = new Request.JSON({
 		url: 'fares.json',
+		type: 'get',
 		onSuccess: function( json, xml ){
-			cosnole.log(json);
-			info 	= json.info;
-			zone_1 	= json.zones[ 0 ];
-			zone_2 	= json.zones[ 1 ];
-			zone_3 	= json.zones[ 2 ];
-			zone_4 	= json.zones[ 3 ];
-			zone_5 	= json.zones[ 4 ];
+			console.log(json);
+			for( key in json.zones[app.zone].fares){
+
+				if( json.zones[app.zone].fares[key].purchase === app.purchase && json.zones[app.zone].fares[key].type === app.type){
+					$( 'numeric-value' ).empty().appendText( json.zones[app.zone].fares[key].price * app.trip );
+				}
+
+			}
 
 
 		}
 	});
 
 	server.send();
-
-	console.log(zone_1);
-	$('numeric-value').empty().appendText(zone_1.fares[0].price);
-
-	$('zone_select').addEvent('change', function(e){
-		var value = e.target.value;
-	});
-
-	$('departure_select').addEvent('change', function(e){
-		var value = e.target.value;
-
-	});
-
-
-	$('onboard_purchase').addEvent('click', function(e){
-		var value = e.target.value;
-
-	});
-
-	$('advance_purchase').addEvent('click', function(e){
-		var value = e.target.value;
-
-	});
-
-	$('numeric').addEvent('keyup', function(e){
-		var value = e.target.value;
-
-	});
-
-});
+}

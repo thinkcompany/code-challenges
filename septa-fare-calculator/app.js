305925b556d031ed266823e1eed13a64a00a6884
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2018-03-13 13:45:59 -0400

    Attach event listeners to form elements

--- septa-fare-calculator/app.js
+++ septa-fare-calculator/app.js
@@ -1,11 +1,17 @@
 'use strict'
 
-// TODO: Declare variables
-
+const zone = document.getElementById("zone_options");
+const fareType = document.getElementById("fare_types");
+const purchaseTypes = [
+  document.getElementById("purchase_location_type_1"),
+  document.getElementById("purchase_location_type_2")
+];
+const tripCount = document.getElementById("trips");
+const displayedFare = document.getElementById("calculated_fare_output");
+const displayedMessage = document.getElementById("message");
 let septaData;
 
-// TODO: Fetch/Request Septa fare data
-
+// FETCH/REQUEST SEPTA FARE DATA
 const fetchData = (filepath, callback) => {
   let request = new XMLHttpRequest();
   request.onreadystatechange = () => {
@@ -25,6 +31,25 @@ fetchData("fares.json", data => {
 
 // TODO: Update UI when events are fired
 
-// TODO: Attach event listeners
+const onChange = () => {
+  console.log('onChange invoked');
+}
+
+// ATTACH EVENT LISTENERS
+const addEventListeners = () => {
+  zone && zone.addEventListener("change", onChange, false);
+  fareType && fareType.addEventListener("change", onChange, false);
+  tripCount && ["input", "change"].map(listener => {
+    tripCount.addEventListener(listener, onChange, false);
+  })
+
+  if (purchaseTypes) {
+    for (let i = 0; i < purchaseTypes.length; i++) {
+      purchaseTypes[i].addEventListener("change", onChange, false);
+    }
+  }
+}
+
+addEventListeners();
 
 // TODO: Dynamically populate UI

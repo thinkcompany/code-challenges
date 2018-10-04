8480103a262175e8fb00f3e190e6bf096530ca5e
kjordan <kjordan@tammantech.com>
2018-10-04 11:11:09 -0400

    Added most styles to calculator. Rendered json values

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import './calculator.css'
 // import data from './../fares.json'
 import logo from '../img/SEPTA.svg'
+import Select from 'react-select'
 
 
 export default class Calculator extends Component {
@@ -10,9 +11,13 @@ export default class Calculator extends Component {
     this.state = {
       error: null,
       isLoaded: false,
+      // fares: [],
+      // info: [],
+      // trips: [],
       data: []
     };
   }
+
 
   componentDidMount() {
     fetch('https://api.myjson.com/bins/ki5sg')
@@ -21,10 +26,15 @@ export default class Calculator extends Component {
         (result => {
           this.setState({
             isLoaded: true,
-            data: result.zones[0].name
-          });
-          console.log(result, 'initial result!')
-          console.log(this.state)
+            // fares: [result.zones],
+            // info: [result.info],
+            // trips: [result.zones[0].fares],
+            data: result
+          })
+          console.log(this.state.data, 'initial result!')
+          // console.log(this.state.fares, 'fares')
+          // console.log(this.state.info, 'info!')
+          // console.log(this.state.trips, 'tripps')
         }),
         (error) => {
           this.setState({
@@ -35,8 +45,13 @@ export default class Calculator extends Component {
       )
     }
 
+
+
   render() {
-    const { error, isLoaded, data } = this.state
+    const { error, isLoaded, data} = this.state
+    console.log(data, 'data down here!')
+    // console.log(data.zones[0].name)
+
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -49,15 +64,41 @@ export default class Calculator extends Component {
               <img alt='septa-logo' src={logo} />
               <h1>Regional Rail Fares</h1>
             </header>
-            <body>
+            <section>
               <h2>Where are you going?</h2>
-            </body>
-            {/* {data} */}
-            {/* {items.map(item => (
-              <h1 key={item.name}>
-                {item.name} {item.price}
-              </h1>
-                ))} */}
+              <select>
+                <option>{data.zones[0].name}</option>
+                <option>{data.zones[1].name}</option>
+                <option>{data.zones[2].name}</option>
+                <option>{data.zones[3].name}</option>
+                <option>{data.zones[4].name}</option>
+              </select>
+            </section>
+            <section>
+              <h2>When are you riding?</h2>
+              <select>
+                <option>{data.zones[0].fares[0].type}</option>
+                <option>{data.zones[1].fares[1].type}</option>
+                <option>{data.zones[2].fares[2].type}</option>
+                <option>{data.zones[3].fares[3].type}</option>
+                <option>{data.zones[4].fares[4].type}</option>
+              </select>
+            </section>
+            <section>
+              <h3>Where will you purchase the fare?</h3>
+              <form action='select'>
+                <input type='radio' name='station kiosk' value='station kiosk' />Station Kiosk<br />
+                <input type='radio' name='on board' value='on board' />On board
+              </form>
+            </section>
+            <section>
+              <h3>How many fares will you need?</h3>
+              <input className='input-integer' type='integer'/>
+            </section>
+            <footer>
+              <h1>Your fare will cost</h1>
+              
+            </footer>
           </div>
         </div>
       );

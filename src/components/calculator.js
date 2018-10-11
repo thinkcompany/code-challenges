3867116e8c011bd9cbc86fee2f972a23435b1962
kjordan <kjordan@tammantech.com>
2018-10-11 18:17:23 -0400

    Added comments for coding decisions, assumptions

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react'
 import './calculator.css'
 import logo from '../img/SEPTA.svg'
 
+
+//Hello, and thank you for looking at my coding challenge. The README said this challenge should take an hour, and as I have already worked on it for several, I've decided I should submit what I have, in hopes that I might have the opportunity to speak with someone about the decisions I made, where I was stuck etc; I have been coding for about a year.
+
 export default class Calculator extends Component {
   constructor(props) {
     super(props)
@@ -38,7 +41,7 @@ export default class Calculator extends Component {
       .then(res => res.json())
       .then(
         (result => {
-          // eslint-disable-next-line
+          //This was my attempt to use es6 destructuring to get into nested data before mapping, but after many tries it didn't work out.
           // const { info, zones: [name, zone, fares : [type, purchase, trips, price ]], error, isLoaded} = result
           this.setState({
             isLoaded: true,
@@ -58,24 +61,33 @@ export default class Calculator extends Component {
       )
     }
 
-    renderInfoCalc () {
-      const {data} = this.state
-      if (!data.length) return
-      return data.map(table => {
-        return (
-          table.map(zone => {
-            console.log(zone)
-            return (
-                zone.map(name => (
-                    // <option>
-                      {name}
-                    // </option>
-                  ))
-                )
-              })
-            )
-          })
-        }
+    renderCalculateFare () {
+      if (this.state.nameStation === 'CCP/1' && this.state.fares < 10) {
+        //I know I need to connect this to the price for each combination, but as of yet have been unable to iterate over the data in a way that makes connecting those values possible.
+      }
+    }
+
+
+ //Below I was trying to map the nested data to populate the dropdowns rather than hard coding it in.
+
+    // renderInfoCalc () {
+    //   const {data} = this.state
+    //   if (!data.length) return
+    //   return data.map(table => {
+    //     return (
+    //       table.map(zone => {
+    //         console.log(zone)
+    //         return (
+    //             zone.map(name => (
+    //                 // <option>
+    //                   {name}
+    //                 // </option>
+    //               ))
+    //             )
+    //           })
+    //         )
+    //       })
+    //     }
 
   render() {
     const {error, isLoaded, data} = this.state
@@ -92,13 +104,16 @@ export default class Calculator extends Component {
               <img alt='septa-logo' src={logo} />
               <h1>Regional Rail Fares</h1>
             </header>
-            <section>
+            {/*
+              My intention here was to call the function above to map the values to the dropdown, but I was unable to get the values to show up. However, there were no errors in the console if this section and function above were included. console.log() also was not hitting, so it was difficult to troubleshoot
+
+              <section>
               <h2>Where are you going?</h2>
               <select value={this.state.fares}
                   onChange={this.handleChange}>
                   <option>{this.renderInfoCalc()}</option>
               </select>
-              </section>
+              </section> */}
               <section>
                 <h2>Where are you going?</h2>
                 <select value={this.state.nameStation} onChange={this.handleChangeName}>
@@ -122,17 +137,19 @@ export default class Calculator extends Component {
                 <form value={this.state.purchase} onToggle={this.handleChange}>
                   <input
                     value={data.info.advance_purchase}
-
-                     onChange={this.handleChange} type='radio' name='station kiosk' />Station Kiosk<br />
+                    onChange={this.handleChange} type='radio' name='station kiosk' />Station Kiosk<br />
                   <input
-                     value={data.zones[2].purchase}
-
-                      onChange={this.handleChange} type='radio' name='on board' />On board
+                    value={data.zones[2].purchase}
+                    onChange={this.handleChange} type='radio' name='on board' />On board
                 </form>
               </section>
               <section>
                 <h3>How many fares will you need?</h3>
-                <input className='input-integer' type='integer'/>
+                <input
+                  value={this.state.fares}
+                  className='input-integer'
+                  type='integer'
+                  onBlur={this.renderCalculateFare()}/>
               </section>
               <footer>
                 <h1>Your fare will cost</h1>

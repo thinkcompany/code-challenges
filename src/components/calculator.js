30b8b8149605b4551a69b37d9bfcd453a301d77b
kjordan <kjordan@tammantech.com>
2018-10-10 22:57:38 -0400

    Trying to map values to first section 'where are you going'

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -15,13 +15,23 @@ export default class Calculator extends Component {
     }
   }
 
-  handleChange (key) {
-  return function (e) {
-    var state = {}
-    state[key] = e.target.value
-    this.setState(state)
-  }.bind(this)
-}
+  handleChange = (event) => {
+    this.setState({
+      fares: event.target.value
+    })
+  }
+
+  handleChangeName = (event) => {
+    this.setState({
+      nameStation: event.target.value
+    })
+  }
+
+  handleChangeTrip = (event) => {
+    this.setState({
+      time: event.target.value
+    })
+  }
 
   componentDidMount() {
     fetch('https://api.myjson.com/bins/ki5sg')
@@ -29,16 +39,15 @@ export default class Calculator extends Component {
       .then(
         (result => {
           // eslint-disable-next-line
-          const { info, zones: [name, zone, fares : [type, purchase, trips, price ]], error, isLoaded} = result
+          // const { info, zones: [name, zone, fares : [type, purchase, trips, price ]], error, isLoaded} = result
           this.setState({
             isLoaded: true,
-            fares: {price},
-            nameStation: {name},
-            purchase: {purchase},
-            time: {info},
+            fares: '',
+            nameStation: '',
+            purchase: '',
+            time: '',
             data: result
           })
-          console.log({price})
         }),
         (error) => {
           this.setState({
@@ -49,16 +58,34 @@ export default class Calculator extends Component {
       )
     }
 
-  render() {
-    const {error, isLoaded, fares, nameStation, purchase, time, data} = this.state
-    // let indexData = [0, 1, 2, 3, 4]
+    renderInfoCalc () {
+      const {data} = this.state
+      if (!data.length) return
+      return data.map(table => {
+        return (
+          table.map(zone => {
+            console.log(zone)
+            return (
+                zone.map(name => (
+                    // <option>
+                      {name}
+                    // </option>
+                  ))
+                )
+              })
+            )
+          })
+        }
 
+  render() {
+    const {error, isLoaded, data} = this.state
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
+      <div>
         <div className='container'>
           <div className='wrapper'>
             <header>
@@ -67,49 +94,53 @@ export default class Calculator extends Component {
             </header>
             <section>
               <h2>Where are you going?</h2>
-
-              <select value={this.state.nameStation} onChange={this.handleChange}>
-                <option>{nameStation}</option>}
-                {/* <option>{data.zones[1].name}</option>
-                <option>{data.zones[2].name}</option>
-                <option>{data.zones[3].name}</option>
-                <option>{data.zones[4].name}</option> */}
+              <select value={this.state.fares}
+                  onChange={this.handleChange}>
+                  <option>{this.renderInfoCalc()}</option>
               </select>
-            </section>
-            <section>
-              <h2>When are you riding?</h2>
-              <select value={this.state.trips} onChange={this.handleChange}>
-                <option>{fares[0].type}</option>
-                {/* <option>{data.zones[1].fares[1].type}</option>
-                <option>{data.zones[2].fares[2].type}</option>
-                <option>{data.zones[3].fares[3].type}</option>
-                <option>{data.zones[4].fares[4].type}</option> */}
-              </select>
-            </section>
-            <section>
-              <h3>Where will you purchase the fare?</h3>
-              <form value={this.state.purchase} onChange={this.handleChange}>
-                <input
-                  // value={info.advance_purchase}
+              </section>
+              <section>
+                <h2>Where are you going?</h2>
+                <select value={this.state.nameStation} onChange={this.handleChangeName}>
+                  <option>{data.zones[0].name}</option>}
+                  <option>{data.zones[1].name}</option>
+                  <option>{data.zones[2].name}</option>
+                  <option>{data.zones[3].name}</option>
+                  <option>{data.zones[4].name}</option>
+                </select>
+              </section>
+              <section>
+                <h2>When are you riding?</h2>
+                <select value={this.state.time} onChange={this.handleChangeTrip}>
+                  <option>{data.zones[2].fares[0].type}</option>
+                  <option>{data.zones[2].fares[2].type}</option>
+                  <option>{data.zones[2].fares[4].type}</option>
+                </select>
+              </section>
+              <section>
+                <h3>Where will you purchase the fare?</h3>
+                <form value={this.state.purchase} onToggle={this.handleChange}>
+                  <input
+                    value={data.info.advance_purchase}
 
-                   onChange={this.handleChange} type='radio' name='station kiosk' />Station Kiosk<br />
-                <input
-                   // value={info.onboard_purchase}
+                     onChange={this.handleChange} type='radio' name='station kiosk' />Station Kiosk<br />
+                  <input
+                     value={data.zones[2].purchase}
 
-                    onChange={this.handleChange} type='radio' name='on board' />On board
-              </form>
-            </section>
-            <section>
-              <h3>How many fares will you need?</h3>
-              <input className='input-integer' type='integer'/>
-            </section>
-            <footer>
-              <h1>Your fare will cost</h1>
-
-            </footer>
+                      onChange={this.handleChange} type='radio' name='on board' />On board
+                </form>
+              </section>
+              <section>
+                <h3>How many fares will you need?</h3>
+                <input className='input-integer' type='integer'/>
+              </section>
+              <footer>
+                <h1>Your fare will cost</h1>
+              </footer>
           </div>
         </div>
-      );
+      </div>
+          )
+        }
+      }
     }
-  }
-}

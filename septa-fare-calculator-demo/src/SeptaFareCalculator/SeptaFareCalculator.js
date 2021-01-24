9eff12c67f7bc964eb5d54d630b6caa43e3d1bf6
Zack McKenna <zackmckenna@gmail.com>
2021-01-24 18:03:29 -0500

    imadded data fetching for fares data from typicode mock server

--- septa-fare-calculator-demo/src/SeptaFareCalculator/SeptaFareCalculator.js
+++ septa-fare-calculator-demo/src/SeptaFareCalculator/SeptaFareCalculator.js
@@ -1,6 +1,22 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const SeptaFareCalculator = () => {
+  const [fareInfo, setFareInfo] = useState(null);
+  const [zones, setZones] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://my-json-server.typicode.com/zackmckenna/fares/db`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFareInfo(data.info);
+        setZones(data.zones);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
+
   return <h1>SeptaFareWidget</h1>;
 };
 

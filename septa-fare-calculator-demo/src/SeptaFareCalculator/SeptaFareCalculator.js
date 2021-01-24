f7af3bb4c699e5b1a01533d2530df0b851389c32
Zack McKenna <zackmckenna@gmail.com>
2021-01-24 18:19:10 -0500

    cocreated component skeleton

--- septa-fare-calculator-demo/src/SeptaFareCalculator/SeptaFareCalculator.js
+++ septa-fare-calculator-demo/src/SeptaFareCalculator/SeptaFareCalculator.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./SeptaFareCalculator.css";
 
 const SeptaFareCalculator = () => {
   const [fareInfo, setFareInfo] = useState(null);
@@ -17,7 +18,35 @@ const SeptaFareCalculator = () => {
       });
   }, []);
 
-  return <h1>SeptaFareWidget</h1>;
+  return (
+    <div className="septaFareCalculator">
+      <h1>Regional Rail Rates</h1>
+      <div>
+        <label>Where are you going?</label>
+        <select>
+          <option>Zone4</option>
+        </select>
+      </div>
+      <div>
+        <label>When are you riding?</label>
+        <select>
+          <option>Weekdays</option>
+        </select>
+      </div>
+      <div>
+        <label>Where will you purchase the fare?</label>
+        <label>
+          <input type="radio" value="onboard" />
+          Onboard
+        </label>
+      </div>
+      <div>
+        <label>How many rides will you need?</label>
+        <input type="number" />
+      </div>
+      <div>Result</div>
+    </div>
+  );
 };
 
 export default SeptaFareCalculator;

9102d19088d5c44c789c79352f58d0ceeca20d8f
Atiya Haider <atiya_haider@hotmail.com>
2021-02-12 13:20:37 -0500

    added polyfill for fetch for IE

--- septa-fare-calculator/script.js
+++ septa-fare-calculator/script.js
@@ -3,22 +3,64 @@ let fareData = [];  //to store json data
 let zones = [];     //to store list of zones
 let inputData = {}; // to store user entered data
 
-//load the json file
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        fareData = data;
 
-        //fill up zones
-        fillZones(fareData.zones);
-    })
-    .catch(err => {
-        alert('Something went wrong: ' + err);
-    })
+// Detect if user is on IE browser
+let isIE = !!window.MSInputMethodContext && !!document.documentMode;
 
-const fillZones = zones => {
+if (isIE) {
+   // Create Promise polyfill script tag
+    let promiseScript = document.createElement("script");
+    promiseScript.type = "text/javascript";
+    promiseScript.src =
+        "https://cdn.jsdelivr.net/npm/promise-polyfill@8.2.0/dist/polyfill.min.js";
+
+  // Create Fetch polyfill script tag
+    var fetchScript = document.createElement("script");
+    fetchScript.type = "text/javascript";
+    fetchScript.src =
+        "https://cdn.jsdelivr.net/npm/whatwg-fetch@3.5.0/dist/fetch.umd.min.js";
+
+  // Add polyfills to head element
+    document.head.appendChild(promiseScript);
+    document.head.appendChild(fetchScript);
+
+  // Wait for the polyfills to load and run the function. 
+    setTimeout(function () {
+        window
+            .fetch(url, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(function(res) { 
+                return res.json() 
+            })
+            .then(handleResponse)
+            .catch(handleErrors);
+        }, 1000);
+} 
+else {
+  // If fetch is supported, just run the fetch function
+    fetch(url)
+        .then(function(res) { 
+            return res.json() 
+        })
+        .then(handleResponse)
+        .catch(handleErrors);
+}
+
+function handleResponse(data) {
+    fareData = data;
+    fillZones(data.zones); //fill up zones
+}
+
+function handleErrors(err) {
+    alert('Something went wrong: ' + err);    
+}
+
+function fillZones(zones) {
     const zone = document.getElementById('zone');
-    zones.map(z => {
+    zones.map(function(z) {
         const option = document.createElement("option");
         option.value = z.zone;
         option.innerHTML = z.name;
@@ -26,7 +68,7 @@ const fillZones = zones => {
     })
 }   
 
-const calculateFare = (e) => {
+function calculateFare(e) {
     const cost = document.getElementById('cost');
     const costText = document.getElementById('cost-text');
 
@@ -35,7 +77,9 @@ const calculateFare = (e) => {
 
     if (validInput(e) && receivedAllData()) {
         // if we have all the information, then filter the correct information from the json data
-        let [zoneData, result] = getFilteredData();
+        let filteredData = getFilteredData();
+        let zoneData = filteredData.zoneData;
+        let result = filteredData.result;
 
         //get the cost element and display calculated price
         if (inputData.time !== 'anytime')
@@ -45,7 +89,9 @@ const calculateFare = (e) => {
 
         //also display price if an upgrade to anytime ticket will be cheaper 
         if (inputData.time !== 'anytime' && parseInt(inputData.rides) === 10) {
-            let anytimeResult = zoneData.fares.filter(({type}) => type === "anytime")                        
+            let anytimeResult = zoneData.fares.filter(function(fare) { 
+                                        return fare.type === "anytime"
+                                })                        
             if (result[0].price * parseInt(inputData.rides) > anytimeResult[0].price) 
                 costText.innerHTML = "Upgrade to Anytime ticket for a special price of $" + 
                                      anytimeResult[0].price + " for 10 tickets, purchased at the Kiosk";
@@ -53,21 +99,22 @@ const calculateFare = (e) => {
     }
 }
 
-const receivedAllData = () => {
+function receivedAllData() {
     if (inputData.zone && inputData.time && inputData.location && inputData.rides)
         return true;
     else
         return false;
 }
 
-const getFilteredData = () => {
-    let zoneData = fareData.zones.filter(z => z.zone === parseInt(inputData.zone))[0];
-    let result = zoneData.fares.filter(({type, purchase}) => 
-                    type === inputData.time && purchase === inputData.location);
-    return [zoneData, result];
+function getFilteredData() {
+    let zoneData = fareData.zones.filter(function(z) { return z.zone === parseInt(inputData.zone)})[0];
+    let result = zoneData.fares.filter(function(fare) {
+                    return fare.type === inputData.time && fare.purchase === inputData.location
+                 });
+    return {"zoneData": zoneData, "result": result};
 }
 
-const validInput = (e) => {
+function validInput(e) {
     if (e.name === "time") {
         //display helper text if timings changed
         const helperText = document.getElementById('helper-text');
@@ -110,8 +157,10 @@ const validInput = (e) => {
     return true;
 }
 
-const validateNumber = (e) => {
+function validateNumber(e) {
     //do not allow values < 1 or > 10 
+    if (isNaN(parseInt(e.value)))
+        e.value = "";
     if (e.value !== "" && e.value < 1)
         e.value = 1;
     if (e.value !== "" && e.value > 10)

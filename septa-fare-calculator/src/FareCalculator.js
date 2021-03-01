0f8176aa3951a27d8e234a4e23bbe006286098da
Carter Chen <carterchen@Carters-MBP.attlocal.net>
2021-03-01 16:07:21 -0500

    app continer

---
+++ septa-fare-calculator/src/FareCalculator.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const FareCalculator = () => {
+  return (
+    <div>
+      <div>Regional Rail Fares</div>
+      <div>Where are you going?</div>
+      <div>When are you riding?</div>
+      <div>Where will you purchase the fare?</div>
+      <div>How many rides will you need?</div>
+    </div>
+  );
+};

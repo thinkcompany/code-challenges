bdd4dde8522ef27f2fd874bb368c4b776581ad01
Nishchay Arya <nishchay.arya92@gmail.com>
2021-03-16 09:29:06 -0400

    Set up widget header

--- src/components/SeptaWidget/index.js
+++ src/components/SeptaWidget/index.js
@@ -1,11 +1,15 @@
 import './SeptaWidget.css'
+import logo from '../../img/logo.svg';
 
 const SeptaWidget = () => {
   return (
-    <div className='septa-widget'>
-      
+    <div className="septa-widget">
+      <div className="septa-widget-header">
+        <img src={logo} alt="septa logo" className="septa-widget-header-logo" />
+        <h1 style={{whiteSpace: 'nowrap'}}>Regional Rail Fares</h1>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SeptaWidget;

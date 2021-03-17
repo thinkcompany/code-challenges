4f65a237a84d6c23f1f1d451dd822aa54ff5e4b6
Grant Kyle <sgrantkyle@gmail.com>
2021-03-17 15:08:41 -0500

    react-select added, options added, effect added to handle select changes and price

--- septa-fare-react-app/src/components/FareWidget/FareWidget.jsx
+++ septa-fare-react-app/src/components/FareWidget/FareWidget.jsx
@@ -1,33 +1,93 @@
 import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import fareData from "../../assets/fares.json";
 import "./styles.css";
 
-import "../../"
+const FareWidget = (props) => {
+  const [showZoneOptions, setShowZoneOptions] = useState("CCP/1");
+  const [selectedTimeOption, setSelectedTimeOption] = useState("");
+  const [radioValue, setRadioValue] = useState("");
+  const [numberOfFares, setNumberOfFares] = useState(1);
+  const timeOptions = [
+    { label: "Week Day", value: "weekday" },
+    { label: "Evening or Weekends", value: "evening_weekend" },
+    { label: "Any TIME!", value: "advance_purchase" },
+  ];
 
-const FareWidget = () => {
-  const [data, setData] = useState([]);
-  const getData = () => {
-    fetch("fares.json", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then(function (response) {
-        console.log(response);
-        return response.json();
-      })
-      .then(function (myJson) {
-        console.log(myJson);
-        setData(myJson);
-      });
+  const zoneOptions = fareData.zones.map(({ name, zone }) => {
+    return { label: name, value: zone };
+  });
+
+  //   const selectedZone = fareData.zones.find(
+  //     (item) => item.zoneOptions === zoneOptions
+  //   );
+  //   const selectedFare = selectedZone.fares.find(
+  //     (item) =>
+  //       item.timeOptions.value === timeOptions.value &&
+  //       item.radioValue === radioValue
+  //   );
+
+  //   console.log("currentZone", selectedFare);
+
+  const parseFareData = JSON.parse(JSON.stringify(fareData));
+
+  //   console.log(
+  //     "fares",
+  //     parseFareData.zones.find(({ zone }) => {
+  //       return showZoneOptions.value === zone;
+  //     })
+  //   );
+
+  const handleNumberOfFares = (value) => {
+    setNumberOfFares(value);
   };
+
   useEffect(() => {
-    getData();
-  }, []);
+    if (zoneOptions && timeOptions && radioValue && numberOfFares) {
+      return parseFareData.zones
+        .find(({ zone }) => {
+          return showZoneOptions.value === zone;
+        })
+        .fares.find(
+          ({ timeOfTrip, advanceOrKiosk }) =>
+            timeOfTrip === timeOptions && advanceOrKiosk === radioValue
+        );
+    }
+  }, [timeOptions, radioValue, numberOfFares]);
 
   return (
     <div>
-      <h1>i am the widget</h1>
+      <div className="fare-widget__container">
+        <Select
+          name="zones"
+          options={zoneOptions}
+          onChange={(value) => {
+            setShowZoneOptions(value);
+          }}
+        />
+        <Select
+          options={timeOptions}
+          onChange={({ value }) => {
+            setSelectedTimeOption(value);
+          }}
+        />
+        <div
+          onChange={(e) => {
+            setRadioValue(e.target.value);
+          }}
+        >
+          <input type="radio" value="onboard_purchase" name="onboard" /> Onboard
+          <input type="radio" value="advance_purchase" name="advance" /> Advance
+        </div>
+        <input
+          type="number"
+          options={numberOfFares}
+          onChange={(e) => {
+            // console.log("setNumberOfFares", e.target.value);
+            handleNumberOfFares(parseInt(e.target.value));
+          }}
+        />
+      </div>
     </div>
   );
 };

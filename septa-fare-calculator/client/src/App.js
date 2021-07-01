06499f6a64d93c729e81a3bad00a5b9672e8530b
Brendan Huffman <bh247484@gmail.com>
2021-07-01 19:46:21 -0400

    Complete styling, structure, and functionality

--- septa-fare-calculator/client/src/App.js
+++ septa-fare-calculator/client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import { ReactComponent as SeptaLogo } from './septa_logo.svg'
+const fareJson = require('./fares.json')
 
 // https://github.com/thinkcompany/code-challenges/blob/master/septa-fare-calculator
 
@@ -17,8 +19,8 @@ function App() {
   //   .then(data => console.log(data))
 
   // Tried AJAX/Fetch request above, it choked on JSON formatting
-  const json = require('./fares.json')
-  const zones = json.zones
+  
+  const zones = fareJson.zones
   const calculateFare = (zone, timeSlot, purchaseLocation, numRides) => {
     let singleFarePrice
     if(numRides > 9 && purchaseLocation === 'advance_purchase'){
@@ -58,53 +60,91 @@ function App() {
     setNumRides(parseInt(e.target.value))
   }
 
+  // Conditional Info
+  let timeSlotText = ''
+  if(timeSlot === 'weekday') {
+    timeSlotText = fareJson.info.weekday
+  } else if (timeSlot === 'evening_weekend') {
+    timeSlotText = fareJson.info.evening_weekend
+  }
+
+  let discountInfo = ''
+  if(numRides > 6 && purchaseLocation === 'advance_purchase') {
+    discountInfo = 'Buying 10+ tickets in advance at a Kiosk? Enjoy a discount!'
+  }
+
   return (
     <div className="App">
       <div className="main-card">
-        <h3>Where are you going?</h3>
-        <select name="zones" onChange={handleZoneSelect} value={zone}>
-          <option value="1">Zone 1</option>
-          <option value="2">Zone 2</option>
-          <option value="3">Zone 3</option>
-          <option value="4">Zone 4</option>
-          <option value="5">Zone 5</option>
-        </select>
-
-        <h3>When are you riding?</h3>
-        <select name="time" onChange={handleTimeSlotSelect} value={timeSlot}>
-          <option value="weekday">Weekday</option>
-          <option value="evening_weekend">Evening or Weekend</option>
-        </select>
-
-        <h3>Where will you purchase the fare?</h3>
-        <div>
-          <input
-              checked={purchaseLocation === 'advance_purchase'}
-              name="purchase_location"
-              onChange={handlePurchaseLocationChange}
-              type="radio"
-              value="advance_purchase"
-          />
-          <label htmlFor="huey">Station Kiosk</label>
+        <div className="top-header">
+          <SeptaLogo />
+          <div className="header-text">Regional Rail Fares</div>
         </div>
+        <div className="card-content">
+          
+          {/* Zone Select */}
+          <div className="content-section">
+            <div className="content-heading">Where are you going?</div>
+            <select name="zones" onChange={handleZoneSelect} value={zone}>
+              <option value="1">Zone 1</option>
+              <option value="2">Zone 2</option>
+              <option value="3">Zone 3</option>
+              <option value="4">Zone 4</option>
+              <option value="5">Zone 5</option>
+            </select>
+          </div>
 
-        <div>
-          <input
-              checked={purchaseLocation === 'onboard_purchase'}
-              name="purchase_location"
-              onChange={handlePurchaseLocationChange}
-              type="radio"
-              value="onboard_purchase"
-          />
-          <label htmlFor="dewey">Onboard</label>
+          {/* TimeSlot Select */}
+          <div className="content-section">
+            <div className="content-heading">When are you riding?</div>
+            <select name="time" onChange={handleTimeSlotSelect} value={timeSlot}>
+              <option value="weekday">Weekday</option>
+              <option value="evening_weekend">Evening or Weekend</option>
+            </select>
+            <div className="timeslot-info">{timeSlotText}</div>
+          </div>
+
+          {/* Radio Buttons */}
+          <div className="content-section">
+            <div className="content-heading">Where will you purchase the fare?</div>
+            <div>
+              <label className="radio-button">
+                <input
+                    checked={purchaseLocation === 'advance_purchase'}
+                    name="purchase_location"
+                    onChange={handlePurchaseLocationChange}
+                    type="radio"
+                    value="advance_purchase"
+                />
+                <span>Station Kiosk</span>
+              </label>
+            </div>                  
+            <div style={{ marginTop: '3px' }}>
+              <label className="radio-button">
+                <input
+                    checked={purchaseLocation === 'onboard_purchase'}
+                    name="purchase_location"
+                    onChange={handlePurchaseLocationChange}
+                    type="radio"
+                    value="onboard_purchase"
+                />
+                <span>Onboard</span>
+              </label>
+            </div>
+          </div>
+          
+          {/* Num Rides Input */}
+          <div className="content-section">
+            <div className="content-heading">How many rides will you need?</div>
+            <input className="num-rides-input" type="number" min="1" max="9999" onChange={handleNumRideChange} value={numRides}></input>
+            <div className="discount-info">{discountInfo}</div>
+          </div>
+
         </div>
-
-        <h3>How many rides will you need?</h3>
-        <input type="number" min="1" onChange={handleNumRideChange} value={numRides}></input>
-
-        <h5>Your fare will cost</h5>
-        {`$${calculatedFare}`}
-
+        <div className="fare-footer">
+          <div className="footer-text">Your fare will cost</div>
+          <div className="final-price">{`$${calculatedFare}`}</div>
+        </div>
       </div>
     </div>
   );

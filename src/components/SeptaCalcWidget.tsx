5694b4f0b0731412906051f53cc564d2ef1c2f87
Raymond Ng <rayng86@gmail.com>
2021-07-03 11:20:11 -0400

    display location purchase, ride quantity, and fare cost components in widget

--- src/components/SeptaCalcWidget.tsx
+++ src/components/SeptaCalcWidget.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import ZoneInput from './ZoneInput';
+import PurchaseLocationInput from './PurchaseLocationInput';
+import RideQuantityInput from './RideQuantityInput';
+import FareCostResult from './FareCostResult';
 
 type SeptaCalcWidgetProps = {
   service: any,
@@ -7,12 +10,22 @@ type SeptaCalcWidgetProps = {
 
 const SeptaCalcWidget = ({ service } : SeptaCalcWidgetProps) => {
     const [zoneValue, setZoneValue] = useState('');
+    const [locationValue, setLocationValue] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [fareCost, setFareCost] = useState('');
 
     return (
       <>
         <div className="flex">
           <ZoneInput zone={zoneValue} setZoneValue={setZoneValue} options={service.payload.zones} />
+          <PurchaseLocationInput
+            locationValue={locationValue}
+            setLocationValue={setLocationValue}
+            info={service.payload.info[locationValue]}
+          />
+          <RideQuantityInput quantity={Number(quantity)} setQuantity={setQuantity} />
         </div>
+        <FareCostResult fareCost={fareCost} />
       </>
     );
 };

33004aeb44bb2c94844db40853191d6ca427ba7f
James Fisher <jfisher396@hotmail.com>
2022-06-21 10:21:08 -0500

    created divs and added headings

--- septa-widget/src/App.js
+++ septa-widget/src/App.js
@@ -1,13 +1,35 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <div className="container">
-      <div className="header">
-        <h1>Regional Rail Fares</h1>
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header">
+          <h1>Regional Rail Fares</h1>
+        </div>
+        <div>
+          <h2>Where are you going?</h2>
+          TODO:add dropdown with zone choices
+        </div>
+        <div>
+          <h2>When are you riding?</h2>
+          TODO:add dropdown with time of ride options
+        </div>
+        <div>
+          <h2>Where will you purchase the fare?</h2>
+          TODO:add select with purchase location options
+        </div>
+        <div>
+          <h2>How many rides will you need?</h2>
+          TODO:add text input for ride quantity
+        </div>
+        <div>
+          <h2>Your fare will cost</h2>
+          <p>$0.00</p>
+        </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;

16f6098bc24bdb806c380bdd5ac0560ed72ee5ac
James Fisher <jfisher396@hotmail.com>
2022-06-21 09:55:23 -0500

    changed title of html page

--- septa-widget/src/App.js
+++ septa-widget/src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;

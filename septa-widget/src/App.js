7f94bb86613810dd5cc34fddec99312f27e35ed1
James Fisher <jfisher396@hotmail.com>
2022-06-21 10:10:03 -0500

    widget container and header rough

--- septa-widget/src/App.js
+++ septa-widget/src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 
 function App() {
   return <div className="App">
-    
+    <div className="container">
+      <div className="header">
+        <h1>Regional Rail Fares</h1>
+      </div>
+    </div>
   </div>;
 }
 

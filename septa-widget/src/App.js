6fb9db06c519e1e53057d8f85856a521565debdd
James Fisher <jfisher396@hotmail.com>
2022-06-21 10:29:12 -0500

    rough styling for option divs

--- septa-widget/src/App.js
+++ septa-widget/src/App.js
@@ -7,24 +7,24 @@ function App() {
         <div className="header">
           <h1>Regional Rail Fares</h1>
         </div>
-        <div>
-          <h2>Where are you going?</h2>
+        <div className="option-div">
+          <h2 className="option-title">Where are you going?</h2>
           TODO:add dropdown with zone choices
         </div>
-        <div>
-          <h2>When are you riding?</h2>
+        <div className="option-div">
+          <h2 className="option-title">When are you riding?</h2>
           TODO:add dropdown with time of ride options
         </div>
-        <div>
-          <h2>Where will you purchase the fare?</h2>
+        <div className="option-div">
+          <h2 className="option-title">Where will you purchase the fare?</h2>
           TODO:add select with purchase location options
         </div>
-        <div>
-          <h2>How many rides will you need?</h2>
+        <div className="option-div">
+          <h2 className="option-title">How many rides will you need?</h2>
           TODO:add text input for ride quantity
         </div>
-        <div>
-          <h2>Your fare will cost</h2>
+        <div className="option-div">
+          <h2 className="option-title">Your fare will cost</h2>
           <p>$0.00</p>
         </div>
       </div>

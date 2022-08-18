068ff7e9882024ae88e90af645b851931b08dc4f
Jason <k.jason0105@gmail.com>
2022-08-18 14:12:10 -0700

    create-react-app

---
+++ septa-fare-calculator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        SEPTA Regional Rail Fare Calculator
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

1f1dd019ba78f552401facab06fa0ef8e4548e23
Cal Brown <calsbrown4@gmail.com>
2022-08-21 21:39:28 -0700

    Dropdown menus are created

--- septa-fare-calculator/src/component/Form.js
+++ septa-fare-calculator/src/component/Form.js
@@ -8,9 +8,9 @@ export default function Form() {
     const res = await fetch(url);
     const data = await res.json()
     setZones(data.zones)
-    setDays(data.days);
+    setDays(data.info);
   }
-  
+ 
   useEffect(()=> {
     //use an empty dependency array so API is called only once
     fetchdata()
@@ -18,9 +18,52 @@ export default function Form() {
   if (!zones.length) {
     return <h2>Loading...</h2>
   } else {
-    debugger;
     return (
-      <div>Form</div>
+      <div>
+        <header>
+          <h1>Regional Rail Fares</h1>
+        </header>
+      <section>
+        <h3>Where are you going?</h3>
+        <label htmlFor="zones">
+          <select name="zones">
+          {zones.map((zone, i) => (
+            /* 
+            I made the list of options dynamic in case more zones were added to the railway system
+            The key is an index rather than set value because the list 
+            of options is static and I am already using the zone# as the value.
+            Default option is zone one
+            */
+            <option key = {i} value={zone.zone}>{zone.name}</option>
+          ))}
+          </select>
+        </label>
+      </section>
+      <section>
+        <h3>When are you riding?</h3>
+        <label htmlFor="days">
+          <select name="days" >
+            {Object.keys(days).slice(0,3).map((day, i) => (
+              <option key = {i} value = {day}>{day}</option>
+            ))}
+          </select>
+        </label>
+      </section>
+      <section>
+        <h3>Where will you purchase the fare?</h3>
+          <div role= 'radio-btn container'>
+          <label htmlFor="">
+            <input type="radio" />Station Kiosk
+          </label>
+          <div role= 'radio-btn container'>
+            <input type="radio" />
+          </div>
+        </div>
+      </section>
+      <section>
+        <h3>Your fare will cost</h3>
+      </section>
+      </div>
     )
 
   }

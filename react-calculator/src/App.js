af028098ee58c2d69ca74a1a1409349e7f84c903
Jackson Noda <starhelios94@gmail.com>
2022-08-23 14:27:15 -0400

    build septa rail fare calculator component

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import FareCalculator from "./components/FareCalculator";
 
 function App() {
   return (
     <div className="App">
+      <FareCalculator />
     </div>
   );
 }

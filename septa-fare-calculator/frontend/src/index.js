b4ce0cf4ce6e1ebf9ebf704167d76c6f9611eebf
Justin Nguyen <judnguyen@ucdavis.edu>
2022-08-25 16:58:57 -0700

    Setup App component

---
+++ septa-fare-calculator/frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {App} from './components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDom.render(<App />, root);
+})

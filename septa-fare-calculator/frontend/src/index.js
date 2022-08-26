505d2cde77a9bbdf7362b312e4318d216704c827
Justin Nguyen <judnguyen@ucdavis.edu>
2022-08-25 17:21:18 -0700

    Get started with App

--- septa-fare-calculator/frontend/src/index.js
+++ septa-fare-calculator/frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {App} from './components/App';
+import {App} from './App';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');

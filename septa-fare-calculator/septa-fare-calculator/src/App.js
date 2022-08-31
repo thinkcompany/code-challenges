e8cce2a5efacce43242f2518c4a334926dc94b8a
Anusha Tuladhar <atuladhar@ucdavis.edu>
2022-08-31 14:46:26 -0700

    copied from previous clone

--- septa-fare-calculator/septa-fare-calculator/src/App.js
+++ septa-fare-calculator/septa-fare-calculator/src/App.js
@@ -1,9 +1,15 @@
+import React from "react";
 
+import CreateFareFrom from "./components/create_fare_form";
+
+import Fares from "./fares.json";
 
 function App() {
+  const faresData = Fares;
+  /* passing data from json as props to CreateFareForm*/
   return (
-    <div className="App">
-     Hello
+    <div>
+      <CreateFareFrom faresData={faresData} />
     </div>
   );
 }

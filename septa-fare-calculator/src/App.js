55fc74a3d3659f957ee19870ccec1fa113fe3123
nick-echevarria <nick.echev@gmail.com>
2022-11-09 15:57:16 -0500

    feat: set up basic architecture + webpack + dev server

---
+++ septa-fare-calculator/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default hot(App);
---
+++ septa-fare-calculator/webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: ['react-hot-loader/patch', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: "development", 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: './dist',
+    },
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+};
+
+module.exports = config;

3588160c4c5d7fb92454c978e68884384fec4704
Tyler Sharp <johntylersharp@gmail.com>
2022-11-17 02:31:05 -0500

    Mostly working, just need CSS

--- septa-fare-calculator/src/js/FareCalculator.js
+++ septa-fare-calculator/src/js/FareCalculator.js
@@ -6,10 +6,63 @@ class FareCalculator {
     numberOfRides,
     fareZoneInfo
   ) {
-    this.whereGoing = whereGoing || "Zone 4";
-    this.whenRiding = whenRiding || "Weekdays";
-    this.purchaseAtOnboard = purchasedAtOnboard || false;
-    this.numberOfRides = numberOfRides || 1;
-    this.fareZoneInfo = fareZoneInfo || {};
+    this.whereGoing = parseInt(whereGoing.split(" ")[1]) || 4;
+    this.whenRiding =
+      !whenRiding === "anytime" || !whenRiding === "weekday"
+        ? "evening_weekend"
+        : whenRiding;
+    this.purchasedAtOnboard = purchasedAtOnboard
+      ? "onboard_purchase"
+      : "advance_purchase";
+    this.numberOfRides = numberOfRides || 4;
+    this.fareZoneInfo = fareZoneInfo || [];
+  }
+
+  setWhereGoing(str) {
+    this.whereGoing = parseInt(str.split(" ")[1]);
+  }
+
+  setWhenRiding(str) {
+    const whenRiding =
+      str === "anytime" || str === "weekday" ? str : "evening_weekend";
+    console.log(str);
+    this.whenRiding = whenRiding;
+  }
+
+  setPurchasedAtOnboard(boolean) {
+    const purchasedAtOnboard = boolean
+      ? "onboard_purchase"
+      : "advance_purchase";
+    this.purchasedAtOnboard = purchasedAtOnboard;
+  }
+
+  setNumberOfRides(num) {
+    this.numberOfRides = num;
+  }
+
+  calculateFare() {
+    let price;
+    const fareZone = this.fareZoneInfo.find((z) => z.zone === this.whereGoing);
+
+    //because everything except anytime and weekday share same pricing, this parses the user's preference to coincide with the json.
+    //however, we kept  the values as user-friendly labels in the html for screen readers
+    const fareType = fareZone.fares.filter((fare) => {
+      return fare.type === this.whenRiding;
+    });
+
+    //we can end the function early if "anytime" is selected, because that's all the info we need to get the user's price - only one anytime price exists per zone
+    if (this.whenRiding === "anytime") {
+      price = fareType[0].price;
+
+      return price;
+    }
+
+    const farePurchase = fareType.find(
+      (fare) => fare.purchase === this.purchasedAtOnboard
+    );
+
+    price = farePurchase.price;
+
+    return price;
   }
 }
--- septa-fare-calculator/src/js/index.js
+++ septa-fare-calculator/src/js/index.js
@@ -1,5 +1,14 @@
+//Could have broken things up into more classes, but for the scope of this assignment didn't seem necessary.
+
 let fareInfo = {};
-let fareZones = {};
+let fareZones = [];
+
+let fareDefaults = {
+  whereGoing: "Zone 4",
+  whenRiding: "weekday",
+  purchasedAtOnboard: false,
+  numberOfRides: 4,
+};
 
 //SETUP
 const getFareData = async () => {
@@ -18,14 +27,103 @@ const setFareData = (fareData) => {
   fareZones = fareData.zones;
 };
 
+const { whereGoing, whenRiding, purchasedAtOnboard, numberOfRides } =
+  fareDefaults;
+
+let WidgetFareCalculator = new FareCalculator(
+  whereGoing,
+  whenRiding,
+  purchasedAtOnboard,
+  numberOfRides
+);
+
 const onLoad = async () => {
   const fareData = await getFareData().catch((error) => error);
 
-  setFareData(fareData);
+  await setFareData(fareData);
 
-  console.log(fareInfo, fareZones);
+  WidgetFareCalculator.fareZoneInfo = fareZones;
 };
 
-//EVENTS
+//EVENT HANDLERS
+const handleWhereGoingSelectChange = (e) => {
+  e.preventDefault();
+
+  const whereGoingValue = e.target.value;
+  WidgetFareCalculator.setWhereGoing(whereGoingValue);
+  displayPrice();
+};
+
+const handleWhenRidingSelectChange = (e) => {
+  //anytime tickets require at least ten rides and adv purchase, so if anytime is selected, it will force it to be on 10 and kiosk if it isn't so.
+  e.preventDefault();
+
+  const whenRidingValue = e.target.value;
+  if (numRidesInput.value < 10 && whenRidingValue == "anytime") {
+    numRidesInput.value = 10;
+  }
+  WidgetFareCalculator.setWhenRiding(whenRidingValue);
+  displayPrice();
+};
+
+const handleKioskOrOnboardButtonsClick = (e) => {
+  const purchasedAtOnboardBoolean = e.target.value === "kiosk" ? false : true;
+  WidgetFareCalculator.setPurchasedAtOnboard(purchasedAtOnboardBoolean);
+  displayPrice();
+};
+
+const handleNumRidesInputChange = (e) => {
+  const numRidesInputValue = e.target.value;
+
+  //because anytime tickets start at 10 rides and adv purchase, if it is above 10, it will default to anytime and kiosk for customer convenience
+  //also disables the input for onboarding purchase after 10, since those must be purchased in advance
+  if (numRidesInputValue >= 10) {
+    whenRidingSelect.value = "anytime";
+    kioskOrOnboardButtons[0].checked = true;
+    kioskOrOnboardButtons[1].disabled = true;
+  }
+
+  if (numRidesInputValue < 10) {
+    kioskOrOnboardButtons[1].disabled = false;
+  }
+
+  WidgetFareCalculator.setNumberOfRides(numRidesInputValue);
+  displayPrice();
+};
+
+//EVENT LISTENERS
+const whereGoingSelect = document.getElementById("where-going-select");
+const whenRidingSelect = document.getElementById("when-riding-select");
+const kioskOrOnboardButtons = document.getElementsByClassName(
+  "kioskOrOnboardRadio"
+);
+const numRidesInput = document.getElementById("num-rides-input");
+
+whereGoingSelect.addEventListener("change", (e) => {
+  handleWhereGoingSelectChange(e);
+});
+
+whenRidingSelect.addEventListener("change", (e) => {
+  handleWhenRidingSelectChange(e);
+});
+
+for (let i = 0; i < kioskOrOnboardButtons.length; i++) {
+  kioskOrOnboardButtons[i].addEventListener("click", (e) => {
+    handleKioskOrOnboardButtonsClick(e);
+  });
+}
+
+numRidesInput.addEventListener("change", (e) => {
+  handleNumRidesInputChange(e);
+});
 
 //HELPERS
+const displayPrice = () => {
+  const price = WidgetFareCalculator.calculateFare();
+
+  const displayPriceSpan = document.getElementsByClassName(
+    "price-calc-output-number"
+  )[0];
+
+  displayPriceSpan.innerHTML = `$${price.toFixed(2)}`;
+};

92ab4b40e918ce21d76375349d28af730a8f6891
Elina McGill <elinamcgill@gmail.com>
2022-12-08 01:15:12 -0500

    Remove redundant code

--- septa-fare-calculator/septa-fare-application/src/components/ApplicationContainer.js
+++ septa-fare-calculator/septa-fare-application/src/components/ApplicationContainer.js
@@ -59,11 +59,6 @@ const ApplicationContainer = props => {
     <StyledContainer>
       <Header header="Regional Rail Fares"/>
       <SectionList/>
-      {/* <Section subheading="Where are you going?" inputType="dropdown"/>
-      <Section subheading="When are you going?" inputType="dropdown" subtext="Helper text that explains the options above."/>
-      <Section subheading="Where will you purchase the fare?" inputType="radio"/>
-      <Section subheading="How many rides will you need?" inputType="number"/>
-      <Section dark subheading="Your fare will cost" text="$28.00"/> */}
     </StyledContainer>
   )
 }

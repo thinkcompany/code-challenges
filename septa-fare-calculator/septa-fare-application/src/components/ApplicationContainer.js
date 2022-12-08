090b381d7ad11b03e8ee92a47f956da3e20816d7
Elina McGill <elinamcgill@gmail.com>
2022-12-08 01:17:28 -0500

    Separate data from logic and display

--- septa-fare-calculator/septa-fare-application/src/components/ApplicationContainer.js
+++ septa-fare-calculator/septa-fare-application/src/components/ApplicationContainer.js
@@ -3,45 +3,13 @@ import Header from './Header';
 import Section from './Section';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { sections } from '../sections';
 
 const StyledContainer = styled.div`
   border: 3px solid #D3D3D3;
   width: 25%;
   margin: 0 auto;
 `;
-
-const sections = [
-  {
-    subheading: "Where are you going?",
-    inputType: "dropdown",
-    dark: false,
-    subtext: null,
-  },
-  {
-    subheading: "When are you going?",
-    inputType: "dropdown",
-    dark: false,
-    subtext: "Helper text that explains the options above."
-  },
-  {
-    subheading: "Where will you purchase the fare?",
-    inputType: "radio",
-    dark: false,
-    subtext: null,
-  },
-  {
-    subheading: "How many rides will you need?",
-    inputType: "number",
-    dark: false,
-    subtext: null,
-  },
-  {
-    subheading: "Your fare will cost",
-    inputType: "",
-    dark: true,
-    subtext: null,
-  },
-];
 
 const SectionList = () => {
   // TODO: comment this function and what's happening here
---
+++ septa-fare-calculator/septa-fare-application/src/sections.js
@@ -0,0 +1,32 @@
+export const sections = [
+    {
+      subheading: "Where are you going?",
+      inputType: "dropdown",
+      dark: false,
+      subtext: null,
+    },
+    {
+      subheading: "When are you going?",
+      inputType: "dropdown",
+      dark: false,
+      subtext: "Helper text that explains the options above."
+    },
+    {
+      subheading: "Where will you purchase the fare?",
+      inputType: "radio",
+      dark: false,
+      subtext: null,
+    },
+    {
+      subheading: "How many rides will you need?",
+      inputType: "number",
+      dark: false,
+      subtext: null,
+    },
+    {
+      subheading: "Your fare will cost",
+      inputType: "",
+      dark: true,
+      subtext: null,
+    },
+  ];

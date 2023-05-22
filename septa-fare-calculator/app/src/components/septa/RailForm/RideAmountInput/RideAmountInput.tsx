1849b047dcf1a49803a779c1d235632fbbb19eb9
superdev0421 <webdev0421@gmail.com>
2023-05-22 13:51:02 -0700

    add ride amount

--- septa-fare-calculator/app/src/components/septa/RailForm/RideAmountInput/RideAmountInput.tsx
+++ septa-fare-calculator/app/src/components/septa/RailForm/RideAmountInput/RideAmountInput.tsx
@@ -1,5 +1,21 @@
-const RideAmountInput = () => {
-	return <></>;
-};
+import Section from "../../../common/Section/Section";
+import "./styles.css";
+
+interface RideAmountProps {
+	amount: number;
+	setAmount: (value: number) => void;
+}
+
+const RideAmountInput: React.FC<RideAmountProps> = ({ amount, setAmount }) => (
+	<Section label="How many rides will you need?">
+		<input
+			className="ride-amount-input"
+			name="Ride Amount"
+			type="number"
+			value={amount}
+			onChange={(e) => setAmount(Number(e.target.value))}
+		/>
+	</Section>
+);
 
 export default RideAmountInput;

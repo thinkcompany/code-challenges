9fa6812975b9aa179f1898e09f15e7440676e71d
superdev0421 <webdev0421@gmail.com>
2023-05-22 11:00:23 -0700

    add septa header

--- septa-fare-calculator/app/src/App.tsx
+++ septa-fare-calculator/app/src/App.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import SeptaFareCalculator from "./container/SeptaFareCalculator/SeptaFareCalculator";
-import logo from "./logo.svg";
-import "./App.css";
+import SeptaHeader from "./components/septa/SeptaHeader/SeptaHeader";
 import useFetchData from "./hooks/useFetchData";
+import "./styles/global.css";
 
 const App = () => {
 	const { data, loading, error } = useFetchData();
-	if (loading) return <>Page Loading...</>;
-	if (error) return <>Page Error</>;
-	return <SeptaFareCalculator />;
+	return (
+		<div className="septa-container">
+			<SeptaHeader />
+			{loading ? (
+				<div>Page Loading...</div>
+			) : error ? (
+				<div>Page Error</div>
+			) : (
+				<SeptaFareCalculator />
+			)}
+		</div>
+	);
 };
 
 export default App;
--- septa-fare-calculator/app/src/components/septa/SeptaHeader/SeptaHeader.tsx
+++ septa-fare-calculator/app/src/components/septa/SeptaHeader/SeptaHeader.tsx
@@ -1,5 +1,12 @@
+import "./styles.css";
+
 const SeptaHeader = () => {
-	return <></>;
+	return (
+		<div className="septa-header bg-gray text-white">
+			<img src="./images/logo.svg" alt="Septa" className="septa-header-logo" />
+			<h2>Regional Rail Fares</h2>
+		</div>
+	);
 };
 
 export default SeptaHeader;

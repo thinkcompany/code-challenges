b2457a6186ac008fa21865cbf336b6d1cff4e212
superdev0421 <webdev0421@gmail.com>
2023-05-22 11:51:43 -0700

    add common radio button

--- septa-fare-calculator/app/src/components/common/RadioButton/RadioButton.tsx
+++ septa-fare-calculator/app/src/components/common/RadioButton/RadioButton.tsx
@@ -1,5 +1,17 @@
-const RadioButton = () => {
-	return <></>;
-};
+import React from "react";
+
+interface RadioButtonProps {
+	checked: boolean;
+	value: string;
+	label?: string;
+	onChange: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const RadioButton: React.FC<RadioButtonProps> = ({ checked, value, label, onChange }) => (
+	<div>
+		<input name={label} type="radio" value={value} checked={checked} onChange={onChange} />
+		<label>{label}</label>
+	</div>
+);
 
 export default RadioButton;

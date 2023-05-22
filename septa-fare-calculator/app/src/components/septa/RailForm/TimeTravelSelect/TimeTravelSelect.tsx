585bb5e475c587b329bd6138e51bbe7edaa86521
superdev0421 <webdev0421@gmail.com>
2023-05-22 11:49:02 -0700

    add time travel

--- septa-fare-calculator/app/src/components/septa/RailForm/TimeTravelSelect/TimeTravelSelect.tsx
+++ septa-fare-calculator/app/src/components/septa/RailForm/TimeTravelSelect/TimeTravelSelect.tsx
@@ -1,5 +1,28 @@
-const TimeTravelSelect = () => {
-	return <></>;
-};
+import React from "react";
+import Section from "../../../common/Section/Section";
+import SelectDropdown from "../../../common/SelectDropDown/SelectDropDown";
+
+interface TimeTravelSelectProps {
+	timeTravel: string;
+	setTimeTravel: (val: string) => void;
+	options: string[];
+	info?: string;
+}
+
+const TimeTravelSelect: React.FC<TimeTravelSelectProps> = ({
+	timeTravel,
+	setTimeTravel,
+	options,
+	info,
+}) => (
+	<Section label="Where are you riding?" info={info}>
+		<SelectDropdown
+			value={timeTravel}
+			placeholder="Select Time Travel"
+			options={options}
+			onSelect={(e) => setTimeTravel(e.target.value)}
+		/>
+	</Section>
+);
 
 export default TimeTravelSelect;

2d1f8da7637824f72932e860e2261526370145d4
superdev0421 <webdev0421@gmail.com>
2023-05-22 13:55:11 -0700

    test purchase location and ride amount

--- septa-fare-calculator/app/src/container/SeptaFareCalculator/SeptaFareCalculator.tsx
+++ septa-fare-calculator/app/src/container/SeptaFareCalculator/SeptaFareCalculator.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import FareResult from "../../components/septa/FareResult/FareResult";
+import PurchaseLocationSelect from "../../components/septa/RailForm/PurchaseLocationSelect/PurchaseLocationSelect";
+import RideAmountInput from "../../components/septa/RailForm/RideAmountInput/RideAmountInput";
 import ZoneSelect from "../../components/septa/RailForm/ZoneSelect/ZoneSelect";
 import { IFaresData } from "../../types/types";
 
@@ -10,10 +12,14 @@ interface SeptaFareCalculatorProps {
 const SeptaFareCalculator: React.FC<SeptaFareCalculatorProps> = ({ faresData }) => {
 	const [fareCost, setFareCost] = useState(0);
 	const [zone, setZone] = useState("");
-	
+	const [location, setLocation] = useState("");
+	const [amount, setAmount] = useState(0);
+
 	return (
 		<>
 			<ZoneSelect zone={zone} setZone={setZone} options={faresData.zones} />
+			<PurchaseLocationSelect location={location} setLocation={setLocation} />
+			<RideAmountInput amount={amount} setAmount={setAmount} />
 			<FareResult fareCost={fareCost} />
 		</>
 	);

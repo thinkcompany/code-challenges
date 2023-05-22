253a387af1a7cea4c2b64bfb847d355df264df56
superdev0421 <webdev0421@gmail.com>
2023-05-22 11:55:30 -0700

    add purchase location select

--- septa-fare-calculator/app/src/components/septa/RailForm/PurchaseLocationSelect/PurchaseLocationSelect.tsx
+++ septa-fare-calculator/app/src/components/septa/RailForm/PurchaseLocationSelect/PurchaseLocationSelect.tsx
@@ -1,5 +1,29 @@
-const PurchaseLocationSelect = () => {
-	return <></>;
-};
+import RadioButton from "../../../common/RadioButton/RadioButton";
+import Section from "../../../common/Section/Section";
+
+interface PurchaseLocationInputProps {
+	location: string;
+	setLocation: (value: string) => void;
+	info?: string;
+}
+
+const PurchaseLocationSelect = ({ location, setLocation, info }: PurchaseLocationInputProps) => (
+	<Section label="Where will you purchase the fare?" info={info}>
+		<div className="radio-buttons-container">
+			<RadioButton
+				label="Station Kiosk"
+				value="advance_purchase"
+				checked={location === "advance_purchase"}
+				onChange={(e) => setLocation(e.target.value)}
+			/>
+			<RadioButton
+				label="Onboard"
+				value="onboard_purchase"
+				checked={location === "onboard_purchase"}
+				onChange={(e) => setLocation(e.target.value)}
+			/>
+		</div>
+	</Section>
+);
 
 export default PurchaseLocationSelect;
